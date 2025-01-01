84770550aa53c76cb0c6e6366760f9a6eb0c2341
note <1@1.com>
2025-01-02 01:33:26 +0900

    1월 2일 테스트 v2

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'github test',
+              'github test2',
             ),
             Text(
               '$_counter',
